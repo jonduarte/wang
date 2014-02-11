538baaeb724c495a10ee7d9600591c482a5d5929
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2014-02-10 23:34:59 -0200

    Add interface with flog to calculate scores

--- lib/wang.rb
+++ lib/wang.rb
@@ -1,2 +1,19 @@
+require 'flog'
 module Wang
+  attr_reader :flog
+
+  class Score
+    def initialize(file)
+      @flog = Flog.new
+      @flog.flog(file)
+    end
+
+    def total
+      @flog.total_score.round(1)
+    end
+
+    def average
+      @flog.average.round(1)
+    end
+  end
 end
--- spec/wang_spec.rb
+++ spec/wang_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Wang do
-  it "is true" do
-    assert true
+  before do
+    @score = Wang::Score.new('spec/fixtures/model.rb')
+  end
+
+  it "calculates total" do
+    assert_equal 18.7, @score.total
+  end
+
+  it "calculates average" do
+    assert_equal 9.3, @score.average
   end
 end
